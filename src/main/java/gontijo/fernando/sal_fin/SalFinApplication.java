58730583d2f566a6ff5c09f73b66c68d20680042
fernandogontijo <fernando.gontijos@hotmail.com>
2024-11-09 16:49:55 +0000

    First commit

---
+++ src/main/java/gontijo/fernando/sal_fin/SalFinApplication.java
@@ -0,0 +1,13 @@
+package gontijo.fernando.sal_fin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalFinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gontijo/fernando/sal_fin/SalFinApplicationTests.java
@@ -0,0 +1,13 @@
+package gontijo.fernando.sal_fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
